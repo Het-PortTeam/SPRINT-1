4dfd67362c3d286aef650c4d5f445f64a9b680a5
RoyvandenHeuvel <0878963@hr.nl>
2014-12-02 14:30:20 +0100

    boeien locatie veranderen

--- Project 2 - Gehele game/Menu/Menu/ControleCentrum.java
+++ Project 2 - Gehele game/Menu/Menu/ControleCentrum.java
@@ -19,8 +19,9 @@ public class ControleCentrum extends World
         super(1280, 720, 1); 
         addObject(new CCLand1(), 100, 495);
         addObject(new CCLand2(), 1180, 565);
-        addObject(new CCBoei(), 500, 300);
-        addObject(new CCBoei(), 800, 300);
+        addObject(new CCBoei(), 400, 300);
+        addObject(new CCBoei(), 650, 300);
+        addObject(new CCBoei(), 900, 300);
         addObject(new CCKade(), 400, 570); 
         addObject(new CCKade(), 650, 570);
         addObject(new CCKade(), 900, 570);
