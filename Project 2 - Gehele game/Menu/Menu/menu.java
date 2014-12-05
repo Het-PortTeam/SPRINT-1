5e296b24b1d2350f337de7c8965bb0eb181080d1
djskin1 <hckevin1990@live.nl>
2014-12-05 13:08:54 +0100

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	Project 2 - Gehele game/Menu/Menu/project.greenfoot

--- Project 2 - Gehele game/Menu/Menu/menu.java
+++ Project 2 - Gehele game/Menu/Menu/menu.java
@@ -5,12 +5,12 @@ public class menu extends World
     public menu()
     {    
         super(1280, 720, 1); 
-        
+      
         Input namePrompt = new Input("Wat is uw naam?");
         String name = namePrompt.getStringInput();
         
         Input ageInput = new Input("Wat is uw leeftijd?");
-        int age= ageInput.getIntInput();
+        int age= ageInput.getIntInput()
         
         addObject(new VOMenu(), 340, 360);
         addObject(new CCMenu(), 740, 520);
