da4c9216253632d6118c45aaf73d6e469b0a0de4
RoyvandenHeuvel <0878963@hr.nl>
2014-12-05 13:11:51 +0100

    yay greenfoot

--- Project 2 - Gehele game/Menu/Menu/menu.java
+++ Project 2 - Gehele game/Menu/Menu/menu.java
@@ -5,13 +5,13 @@ public class menu extends World
     public menu()
     {    
         super(1280, 720, 1); 
-      
+        /*
         Input namePrompt = new Input("Wat is uw naam?");
         String name = namePrompt.getStringInput();
         
         Input ageInput = new Input("Wat is uw leeftijd?");
-        int age= ageInput.getIntInput()
-        
+        int age= ageInput.getIntInput();
+        */
         addObject(new VOMenu(), 340, 360);
         addObject(new CCMenu(), 740, 520);
     }
