e80c5a58eb36f1fd9a9e51f67c375c775a9be414
oveldman <oveldman@gmail.com>
2014-12-06 13:19:15 +0100

    VO change

--- Project 2 - Gehele game/Menu/Menu/VrachtOverslaan.java
+++ Project 2 - Gehele game/Menu/Menu/VrachtOverslaan.java
@@ -10,11 +10,17 @@ public class VrachtOverslaan extends World
         addObject(new Rails(), 640, 360);
         addObject(new VOSchip(), 640,440);
         addObject(new VOSchip2(), 640, 250);
+        // De containers:
         addObject(new VOConB(), 990, 400);
         addObject(new VOConM(), 1005, 443 );
         addObject(new VOConS(), 1020, 485);
+        addObject(new VOConB(), 945, 485);
+        addObject(new VOConS(), 945, 443);
+        addObject(new VOConM(), 900, 400);
+        // Tot hier!
         addObject(new VOGrijper(), 640,360);
         addObject(new VOCrane(), 640, 360);
+        addObject(new VOGrijper2(), 500, 130);
         addObject(new VOCrane2(), 500, 360);
     }
 }
