fa2f7016d871e22cfce4548c9767f07fe02a2031
RoyvandenHeuvel <0878963@hr.nl>
2014-12-12 13:17:39 +0100

    snelle fix klein probleempje met > en < tekens bij bewegen

--- Project 2 - Gehele game/Menu/Menu/CCSchip.java
+++ Project 2 - Gehele game/Menu/Menu/CCSchip.java
@@ -15,26 +15,30 @@ public class CCSchip extends Actor
                 int mX = mouse.getX();
                 int mY = mouse.getY();
                 if ( mouse != null && mX > 220 && mX < 340 && mY > 440 && mY < 725) {
-                    if( x > 281) { setLocation(getX() -2, getY() );} 
+                    if( x > 280) { setLocation(getX() -1, getY() );}
+                    if( x < 280) { setLocation(getX() +1, getY() );}
                     if( x == 280) { setRotation(270); setLocation(getX(), getY()+1 ); }
                     if( x == 280 && y == 590) { setLocation(280, 590 ); }
                 }
  
                 else{if ( mouse != null && mX > 468 && mX < 588 && mY > 440 && mY < 725) {
-                    if( x > 528) { setLocation(getX() -2, getY() );} 
+                    if( x > 528) { setLocation(getX() -1, getY() );}
+                    if( x < 528) { setLocation(getX() +1, getY() );}
                     if( x == 528) { setRotation(270); setLocation(getX(), getY()+1 ); }
                     if( x == 528 && y == 590) { setLocation(528, 590 ); }
                 }
     
  
                 else{if( mouse != null && mX > 716 && mX < 836 && mY > 440 && mY < 725) {
-                    if( x > 776) { setLocation(getX() -2, getY() );} 
+                    if( x > 776) { setLocation(getX() -1, getY() );} 
+                    if( x < 776) { setLocation(getX() +1, getY() );}
                     if( x == 776) { setRotation(270); setLocation(getX(), getY()+1 ); }
                     if( x == 776 && y == 590) { setLocation(776, 590 ); }
                 }
     
                 else{if( mouse != null && mX > 964 && mX < 1084 && mY > 440 && mY < 725) {
-                    if( x > 1024) { setLocation(getX() -2, getY() );} 
+                    if( x > 1024) { setLocation(getX() -1, getY() );} 
+                    if( x < 1024) { setLocation(getX() +1, getY() );}
                     if( x == 1024) { setRotation(270); setLocation(getX(), getY()+1 ); }
                     if( x == 1024 && y == 590) { setLocation(1024, 590 ); }
                 }
