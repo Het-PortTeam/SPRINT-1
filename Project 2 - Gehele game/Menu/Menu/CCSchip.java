b2adaac059b15b61ec59e5a8e7447a992c9628e9
JHNBOS <bosbosjohan@gmail.com>
2014-12-12 17:15:30 +0100

    CCSchip boolean ShipInHarbor

--- Project 2 - Gehele game/Menu/Menu/CCSchip.java
+++ Project 2 - Gehele game/Menu/Menu/CCSchip.java
@@ -14,6 +14,9 @@ public class CCSchip extends Actor
      * Act - do whatever the CCSchip wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    private boolean ShipInHarbor;
+    
+    
     public void act() 
     {
         MouseInfo mouse = Greenfoot.getMouseInfo();
@@ -23,34 +26,49 @@ public class CCSchip extends Actor
         int x = getX();
         int y = getY();
  
-   if ( mouse != null && mX > 220 && mX < 340 && mY > 440 && mY < 725) {
-   if( x > 281) { setLocation(getX() -2, getY() );} 
-   if( x == 280) { setRotation(270); setLocation(getX(), getY()+1 ); }
-   if( x == 280 && y == 590) { setLocation(280, 590 ); }
- }
+        
+    if(!ShipInHarbor){     
+    if ( mouse != null && mX > 220 && mX < 340 && mY > 440 && mY < 725) {
+    if( x > 281) { setLocation(getX() -2, getY() );} 
+    if( x == 280) { setRotation(270); setLocation(getX(), getY()+1 ); }
+    }}
+
+    if(x == 280 && y == 590){ShipInHarbor = true;}
  
-else if ( mouse != null && mX > 468 && mX < 588 && mY > 440 && mY < 725) {
-   if( x > 528) { setLocation(getX() -2, getY() );} 
-   if( x == 528) { setRotation(270); setLocation(getX(), getY()+1 ); }
-   if( x == 528 && y == 590) { setLocation(528, 590 ); }
- }
+    if(!ShipInHarbor){
+    if ( mouse != null && mX > 468 && mX < 588 && mY > 440 && mY < 725) {
+    if( x > 528) { setLocation(getX() -2, getY() );} 
+    if( x == 528) { setRotation(270); setLocation(getX(), getY()+1 ); }
+    }}
+
+    if(x == 528 && y == 590){ShipInHarbor = true;}
  
- else{if( mouse != null && mX > 716 && mX < 836 && mY > 440 && mY < 725) {
-   if( x > 776) { setLocation(getX() -2, getY() );} 
-   if( x == 776) { setRotation(270); setLocation(getX(), getY()+1 ); }
-   if( x == 776 && y == 590) { setLocation(776, 590 ); }
- }
+    if(!ShipInHarbor){
+    if( mouse != null && mX > 716 && mX < 836 && mY > 440 && mY < 725) {
+    if( x > 776) { setLocation(getX() -2, getY() );} 
+    if( x == 776) { setRotation(270); setLocation(getX(), getY()+1 ); }
+    }}
  
-  else{if( mouse != null && mX > 964 && mX < 1084 && mY > 440 && mY < 725) {
-   if( x > 1024) { setLocation(getX() -2, getY() );} 
-   if( x == 1024) { setRotation(270); setLocation(getX(), getY()+1 ); }
-   if( x == 1024 && y == 590) { setLocation(1024, 590 ); }
- }
- 
+    if(x == 776 && y == 590){ShipInHarbor = true;}
+
+    if(!ShipInHarbor){
+    if( mouse != null && mX > 964 && mX < 1084 && mY > 440 && mY < 725) {
+    if( x > 1024) { setLocation(getX() -2, getY() );} 
+    if( x == 1024) { setRotation(270); setLocation(getX(), getY()+1 ); }
+    }}
+   
+    if(x == 1024 && y == 590){ShipInHarbor = true;}
+    if(x == 1024 && y == 590) { ShipInHarbor = true;}
+    if(x == 776 && y == 590) { ShipInHarbor = true;}
+    if(x == 528 && y == 590) { ShipInHarbor = true;}
+    if(x == 280 && y == 590) { ShipInHarbor = true;}
+   
+    if(ShipInHarbor == true) {setLocation(x, y );}
     }
   }
- }
-}
+
+
+
 
 
 
