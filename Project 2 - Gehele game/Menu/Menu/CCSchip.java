c9098869ae875c0c4d2235073236da0a7dfd5c47
JHNBOS <bosbosjohan@gmail.com>
2014-12-14 15:28:21 +0100

    TROLOLOLOLOL

--- Project 2 - Gehele game/Menu/Menu/CCSchip.java
+++ Project 2 - Gehele game/Menu/Menu/CCSchip.java
@@ -28,33 +28,33 @@ public class CCSchip extends Actor
  
         
     if(!ShipInHarbor){     
-    if ( mouse != null && mouseX > 220 && mouseX < 340 && mouseY > 440 && mouseY < 725) {
+    if (mouseX > 220 && mouseX < 340 && mouseY > 440 && mouseY < 725) {
     if( x > 281) { setLocation(getX() -2, getY() );} 
-    if( x == 280) { setRotation(270); setLocation(getX(), getY()+1 ); }
+    if( x == 280) { setRotation(270); setLocation(x, y+1 ); }
     }}
 
     if(x == 280 && y == 590){ShipInHarbor = true;}
  
     if(!ShipInHarbor){
-    if ( mouse != null && mouseX > 468 && mouseX < 588 && mouseY > 440 && mouseY < 725) {
+    if (mouseX > 468 && mouseX < 588 && mouseY > 440 && mouseY < 725) {
     if( x > 528) { setLocation(getX() -2, getY() );} 
-    if( x == 528) { setRotation(270); setLocation(getX(), getY()+1 ); }
+    if( x == 528 ) { setRotation(270); setLocation(x, y+1 ); }
     }}
 
     if(x == 528 && y == 590){ShipInHarbor = true;}
  
     if(!ShipInHarbor){
-    if( mouse != null && mouseX > 716 && mouseX < 836 && mouseY > 440 && mouseY < 725) {
+    if(mouseX > 716 && mouseX < 836 && mouseY > 440 && mouseY < 725) {
     if( x > 776) { setLocation(getX() -2, getY() );} 
-    if( x == 776) { setRotation(270); setLocation(getX(), getY()+1 ); }
+    if( x == 776) { setRotation(270); setLocation(x, y+1 ); }
     }}
  
     if(x == 776 && y == 590){ShipInHarbor = true;}
 
     if(!ShipInHarbor){
-    if( mouse != null && mouseX > 964 && mouseX < 1084 && mouseY > 440 && mouseY < 725) {
+    if(mouseX > 964 && mouseX < 1084 && mouseY > 440 && mouseY < 725) {
     if( x > 1024) { setLocation(getX() -2, getY() );} 
-    if( x == 1024) { setRotation(270); setLocation(getX(), getY()+1 ); }
+    if( x == 1024) { setRotation(270); setLocation(x, y+1 ); }
     }}
    
     if(x == 1024 && y == 590){ShipInHarbor = true;}
