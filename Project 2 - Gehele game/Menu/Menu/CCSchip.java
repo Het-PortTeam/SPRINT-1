d8fc0dc582be20014297c7430008e3329ecf9dc8
JHNBOS <bosbosjohan@gmail.com>
2014-12-16 15:27:05 +0100

    boot, BLIJF!

--- Project 2 - Gehele game/Menu/Menu/CCSchip.java
+++ Project 2 - Gehele game/Menu/Menu/CCSchip.java
@@ -30,32 +30,32 @@ public class CCSchip extends Actor
         
     if(!ShipInHarbor){     
     if (mouseX > 220 && mouseX < 340 && mouseY > 440 && mouseY < 725) {
-    if( x > 281) { setLocation(getX() -2, getY() );} 
-    if( x == 280) { setRotation(270); setLocation(x, y+1 ); }
+    if( x > 281 && y == 200) { setLocation(getX() -2, getY() );} 
+    if( x == 280) { setRotation(270); setLocation(getX(), y+1 ); }
     }}
 
     if(x == 280 && y == 590){ShipInHarbor = true;}
  
     if(!ShipInHarbor){
     if (mouseX > 468 && mouseX < 588 && mouseY > 440 && mouseY < 725) {
-    if( x > 528) { setLocation(getX() -2, getY() );} 
-    if( x == 528 ) { setRotation(270); setLocation(x, y+1 ); }
+    if( x > 528 && y == 200) { setLocation(getX() -2, getY() );} 
+    if( x == 528 ) { setRotation(270); setLocation(getX(), y+1 ); }
     }}
 
     if(x == 528 && y == 590){ShipInHarbor = true;}
  
     if(!ShipInHarbor){
     if(mouseX > 716 && mouseX < 836 && mouseY > 440 && mouseY < 725) {
-    if( x > 776) { setLocation(getX() -2, getY() );} 
-    if( x == 776) { setRotation(270); setLocation(x, y+1 ); }
+    if( x > 776 && y == 200) { setLocation(getX() -2, getY() );} 
+    if( x == 776 ) { setRotation(270); setLocation(getX(), y+1 ); }
     }}
  
     if(x == 776 && y == 590){ShipInHarbor = true;}
 
     if(!ShipInHarbor){
     if(mouseX > 964 && mouseX < 1084 && mouseY > 440 && mouseY < 725) {
-    if( x > 1024) { setLocation(getX() -2, getY() );} 
-    if( x == 1024) { setRotation(270); setLocation(x, y+1 ); }
+    if( x > 1024 && y == 200) { setLocation(getX() -2, getY() );} 
+    if( x == 1024) { setRotation(270); setLocation(getX(), y+1 ); }
     }}
    
     if(x == 1024 && y == 590){ShipInHarbor = true;}
