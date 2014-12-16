3cb67313d5d4ab47dfcf9e0d1e2607c8487d9e81
RoyvandenHeuvel <0878963@hr.nl>
2014-12-16 13:57:06 +0100

    its a whole new world

---
+++ Project 2 - Gehele game/Menu/Menu/BallastEngineer.java
@@ -0,0 +1,21 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class BallastEngineer here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class BallastEngineer extends World
+{
+
+    /**
+     * Constructor for objects of class BallastEngineer.
+     * 
+     */
+    public BallastEngineer()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(1280, 720, 1); 
+    }
+}
