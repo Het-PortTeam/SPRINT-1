5768e18d1f0db318c3945fa54955624444bbc1c6
RoyvandenHeuvel <0878963@hr.nl>
2014-12-19 13:36:47 +0100

    fwoosh

--- Project 2 - Gehele game/Menu/Menu/menu.java
+++ Project 2 - Gehele game/Menu/Menu/menu.java
@@ -14,7 +14,7 @@ public class menu extends World
          */
         addObject(new VOMenu(), 250, 310);
         addObject(new CCMenu(), 740, 220);
-        addObject(new BEMenu(), 500, 500);
+        addObject(new BEMenu(), 500, 180);
         addObject(new DMenu(), 700, 50);
         //addObject(new HMenu(), 900, 500);
 
