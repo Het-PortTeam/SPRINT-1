f4bae78103b5958d9c8f31af4326b4b1e23d03c5
RoyvandenHeuvel <0878963@hr.nl>
2014-12-30 15:08:42 +0100

    CCplek transparant 255/255

--- Project 2 - Gehele game/Menu/Menu/CCplek.java
+++ Project 2 - Gehele game/Menu/Menu/CCplek.java
@@ -14,7 +14,7 @@ public class CCplek extends Actor
      */
     public void act() 
     {
-        getImage().setTransparency(100);
+        getImage().setTransparency(255);
         
         
         
