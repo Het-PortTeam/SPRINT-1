1f32c262a2ae62e9509571b39a40eb62274e53fc
RoyvandenHeuvel <0878963@hr.nl>
2015-01-03 14:21:16 +0100

    formatting en poging score

--- Project 2 - Gehele game/Menu/Menu/CCSchip.java
+++ Project 2 - Gehele game/Menu/Menu/CCSchip.java
@@ -44,8 +44,18 @@ public class CCSchip extends Actor
         if (isAtEdge())  
         {  
              getWorld().removeObject(this);  
+             //addScore();
         }  
     }
+    /*
+    public void addScore()
+    {
+        if (!getWorld().getObjects(Counter.class).isEmpty())
+        {
+            ((Counter) getWorld().getObjects(Counter.class).get(0)).add(1);
+        }
+    }
+    */
      public void move()
      {
         MouseInfo mouse = Greenfoot.getMouseInfo();
@@ -61,66 +71,66 @@ public class CCSchip extends Actor
         Actor eind = getOneIntersectingObject( CCEind.class );
         
         
-        if(Greenfoot.mouseClicked(this)){shipclick = true;}
+            if(Greenfoot.mouseClicked(this)){shipclick = true;}
         
-        if(shipclick == true){
+            if(shipclick == true){
 
-    if(!ShipInHarbor){
-    if(mouseX > 221 && mouseX < 341 && mouseY > 440 && mouseY < 725 && Greenfoot.mouseClicked(plek) ){clicked1 = true;}
-    if(clicked1 == true){
-    if( x > 281) { setLocation(getX() -2, getY() );} 
-    if( x == 280 && y >= 200) { setRotation(270); setLocation(x, y+1 ); }}}
+                if(!ShipInHarbor){
+                    if(mouseX > 221 && mouseX < 341 && mouseY > 440 && mouseY < 725 && Greenfoot.mouseClicked(plek) ){clicked1 = true;}
+                    if(clicked1 == true){
+                        if( x > 281) { setLocation(getX() -2, getY() );} 
+                        if( x == 280 && y >= 200) { setRotation(270); setLocation(x, y+1 ); }}}
     
 
-    if(x == 280 && y == 590){ShipInHarbor = true;}
+                        if(x == 280 && y == 590){ShipInHarbor = true;}
  
 
-    if(!ShipInHarbor){
-    if(mouseX > 468 && mouseX < 588 && mouseY > 440 && mouseY < 725 && Greenfoot.mouseClicked(plek) ){clicked2 = true;}
-    if(clicked2 == true){
-    if( x > 528) { setLocation(getX() -2, getY() );} 
-    if( x == 528 && y >= 200) { setRotation(270); setLocation(x, y+1 ); }}}
+                        if(!ShipInHarbor){
+                            if(mouseX > 468 && mouseX < 588 && mouseY > 440 && mouseY < 725 && Greenfoot.mouseClicked(plek) ){clicked2 = true;}
+                            if(clicked2 == true){
+                                if( x > 528) { setLocation(getX() -2, getY() );} 
+                                if( x == 528 && y >= 200) { setRotation(270); setLocation(x, y+1 ); }}}
     
 
-    if(x == 528 && y == 590){ShipInHarbor = true;}
+                                if(x == 528 && y == 590){ShipInHarbor = true;}
             
     
-    if(!ShipInHarbor){   
-    if(mouseX > 716 && mouseX < 836 && mouseY > 440 && mouseY < 725 && Greenfoot.mouseClicked(plek) ){clicked3 = true;}
-    if(clicked3 == true){
-    if( x > 776) { setLocation(getX() -2, getY() );} 
-    if( x == 776 && y >= 200) { setRotation(270); setLocation(x, y+1 ); }}}
+                                if(!ShipInHarbor){   
+                                    if(mouseX > 716 && mouseX < 836 && mouseY > 440 && mouseY < 725 && Greenfoot.mouseClicked(plek) ){clicked3 = true;}
+                                    if(clicked3 == true){
+                                        if( x > 776) { setLocation(getX() -2, getY() );} 
+                                        if( x == 776 && y >= 200) { setRotation(270); setLocation(x, y+1 ); }}}
    
  
-    if(x == 776 && y == 590){ShipInHarbor = true;}
+                                        if(x == 776 && y == 590){ShipInHarbor = true;}
 
             
-    if(!ShipInHarbor){
-    if(mouseX > 964 && mouseX < 1084 && mouseY > 440 && mouseY < 725 && Greenfoot.mouseClicked(plek) ){clicked4 = true;}
-    if(clicked4 == true){
-    if( x > 1024) { setLocation(getX() -2, getY() );} 
-    if( x == 1024 && y >= 200) { setRotation(270); setLocation(x, y+1 ); }}}
+                                        if(!ShipInHarbor){
+                                            if(mouseX > 964 && mouseX < 1084 && mouseY > 440 && mouseY < 725 && Greenfoot.mouseClicked(plek) ){clicked4 = true;}
+                                            if(clicked4 == true){
+                                                if( x > 1024) { setLocation(getX() -2, getY() );} 
+                                                if( x == 1024 && y >= 200) { setRotation(270); setLocation(x, y+1 ); }}}
    
     
     
     
-    if(x == 1024 && y == 590){ShipInHarbor = true; /*W.removeObjects(W.getObjects(CCSchip.class));*/}
-    if(x == 776 && y == 590) { ShipInHarbor = true; /*W.removeObjects(W.getObjects(CCSchip.class));*/}
-    if(x == 528 && y == 590) { ShipInHarbor = true; /*W.removeObjects(W.getObjects(CCSchip.class));*/}
-    if(x == 280 && y == 590) { ShipInHarbor = true; /*W.removeObjects(W.getObjects(CCSchip.class));*/}
+                                                if(x == 1024 && y == 590){ShipInHarbor = true; /*W.removeObjects(W.getObjects(CCSchip.class));*/}
+                                                if(x == 776 && y == 590) { ShipInHarbor = true; /*W.removeObjects(W.getObjects(CCSchip.class));*/}
+                                                if(x == 528 && y == 590) { ShipInHarbor = true; /*W.removeObjects(W.getObjects(CCSchip.class));*/}
+                                                if(x == 280 && y == 590) { ShipInHarbor = true; /*W.removeObjects(W.getObjects(CCSchip.class));*/}
 
-    if(ShipInHarbor == true) {setLocation(x, y );} 
+                                                if(ShipInHarbor == true) {setLocation(x, y );} 
     
 
-    if(ShipInHarbor == true){
-    if(mouseX > 76 && mouseX < 196 && mouseY > 162 && mouseY < 262 && Greenfoot.mouseClicked(eind) ){back = true;}
-    if(back == true){ setLocation(getX(), getY()-2 );}
-    if((x <= 1024 && y == 200) || (x <= 776 && y == 200) || (x <= 528 && y == 200) || (x <= 280 && y == 200)){setRotation(0); setLocation(getX() -2, y );}}
+                                                if(ShipInHarbor == true){
+                                                    if(mouseX > 76 && mouseX < 196 && mouseY > 162 && mouseY < 262 && Greenfoot.mouseClicked(eind) ){back = true;}
+                                                    if(back == true){ setLocation(getX(), getY()-2 );}
+                                                    if((x <= 1024 && y == 200) || (x <= 776 && y == 200) || (x <= 528 && y == 200) || (x <= 280 && y == 200)){setRotation(0); setLocation(getX() -2, y );}}
     
     
 
-  }
-  if(x <= 136 && y == 200){counter.add(5); }
-}
-}
+            }
+            if(x <= 136 && y == 200){counter.add(5); }
+        }
+    }
 }
