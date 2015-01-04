eb46a60f9320bc0349aaf55f1a2e8d730365de35
oveldman <oveldman@gmail.com>
2015-01-04 21:00:59 +0100

    Schip

--- Project 2 - Gehele game/Menu/Menu/VOSchip.java
+++ Project 2 - Gehele game/Menu/Menu/VOSchip.java
@@ -8,12 +8,13 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class VOSchip extends Actor
 {
+    int gewicht = 100;
     /**
      * Act - do whatever the VOSchip wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act() 
     {
-        // Add your action code here.
+        
     }    
 }
