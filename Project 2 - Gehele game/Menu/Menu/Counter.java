09aa1b9c2e399de62a099cac27b31e9858761b8a
djskin1 <hckevin1990@live.nl>
2015-01-06 12:01:58 +0100

    Score
    score back

--- Project 2 - Gehele game/Menu/Menu/Counter.java
+++ Project 2 - Gehele game/Menu/Menu/Counter.java
@@ -38,6 +38,7 @@ public class Counter extends Actor
     private int value;
     private int target;
     private String prefix;
+    int score = 0;
     
     public Counter()
     {
@@ -125,4 +126,15 @@ public class Counter extends Actor
                         (image.getHeight()-text.getHeight())/2);
         setImage(image);
     }
+    
+    /**
+     * Add score
+     */
+     public void addScore(){
+         if(score < 19){
+             score++;
+            }else{
+                System.out.print("U heeft Gewonnen!");
+            }
+        }
 }
