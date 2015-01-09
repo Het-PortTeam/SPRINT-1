45efa6aeb2f4271704a7fb208d1cf85134f57e9a
djskin1 <hckevin1990@live.nl>
2015-01-09 21:04:19 +0100

    nog met null pointer maar we komen er
    container tegenstander bewegen

--- Project 2 - Gehele game/Menu/Menu/VOConEM2.java
+++ Project 2 - Gehele game/Menu/Menu/VOConEM2.java
@@ -8,12 +8,25 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class VOConEM2 extends VOConEM
 {
+    World myWorld = getWorld();
+    VrachtOverslaan VrachtOverslaan = (VrachtOverslaan)myWorld;
+    private int xSpeed = 2;
+    private int vSpeed = 2;
+    
     /**
      * Act - do whatever the VOConEM2 wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act() 
     {
-        // Add your action code here.
+        if(!getObjectsInRange(25, VOgrijper20.class).isEmpty() && !VrachtOverslaan.grijper.grab){
+            VrachtOverslaan.grijper.grab = true;
+            setLocation(getX() +xSpeed, getY() +vSpeed);
+        }
+        
+        if(isAtEdge()){
+            VrachtOverslaan.grijper.grab = false;
+            myWorld.removeObject(this);
+        }
     }    
 }
