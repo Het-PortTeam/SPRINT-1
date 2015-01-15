82c418092260de5135ec96081dc7e18a53178cb9
RoyvandenHeuvel <0878963@hr.nl>
2015-01-15 11:34:08 +0100

    I AM THE LAW

--- Project 2 - Gehele game/Menu/Menu/DVracht.java
+++ Project 2 - Gehele game/Menu/Menu/DVracht.java
@@ -1,4 +1,5 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import java.util.Random;
 
 /**
  * Write a description of class DVracht here.
@@ -13,9 +14,68 @@ public class DVracht extends Actor
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     boolean Illegal;
+    int PointValue;
     
-    public void act() 
+    public DVracht(boolean Illegal, int PointValue)
     {
-        // Add your action code here.
-    }    
+         setIllegal(Illegal);
+         setImage();
+    }
+    
+    public void setImage()
+    {
+        int randFour = randInt(1, 4);
+        int randThree = randInt(1, 3);
+        if(getIllegal() == true)
+        {            
+            if(randFour == 1){
+                setImage(new GreenfootImage("Vracht1.png"));
+            }
+            if(randFour == 2){
+                setImage(new GreenfootImage("Vracht3.png"));
+            }
+            if(randFour == 3){
+                setImage(new GreenfootImage("Vracht6.png"));
+            }
+            if(randFour == 4){
+                setImage(new GreenfootImage("Vracht7.png"));
+            }
+        }
+        else
+        {
+            if(randThree == 1){
+                setImage(new GreenfootImage("Vracht4.png"));
+            }
+            if(randThree == 2){
+                setImage(new GreenfootImage("Vracht2.png"));
+            }
+            if(randThree == 3){
+                setImage(new GreenfootImage("Vracht5.png"));
+            }
+        }
+    }
+    
+    public static int randInt(int min, int max) {
+        Random rand = new Random();
+    
+        int randomNum = rand.nextInt((max - min) + 1) + min;
+    
+        return randomNum;
+    }
+    
+    public boolean getIllegal()
+    {
+        return Illegal;
+    }
+    
+    public boolean setIllegal(boolean theLaw)
+    {
+        Illegal = theLaw;
+        return Illegal;
+    }
+    
+    public int getPointValue()
+    {
+        return PointValue;
+    }
 }
